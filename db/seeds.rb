d2ffabe395f539f9cd824cccd44eb9a18dd4637a
SaturdayAM <jwl62@georgetown.edu>
2018-05-09 17:01:45 -0400

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-user_one = User.create(username: "Satoshi Nakamoto");
-user_two = User.create(username: "Vitalik Butterin");
-user_three = User.create(username: "Jamie Dimon");
-user_four = User.create(username: "I.C. Weiner");
-user_five = User.create(username: "Phillip J. Fry");
+user_one = User.create(username: "Satoshi_Nakamoto");
+user_two = User.create(username: "Vitalik_Butterin");
+user_three = User.create(username: "Jamie_Dimon");
+user_four = User.create(username: "Doge");
+user_five = User.create(username: "Phillip_J_Fry");
 
 message_1 = Message.create(user_id: user_one.id, content: "This message is from Satoshi");
 message_2 = Message.create(user_id: user_two.id, content: "This message is from Butterin");
 message_3 = Message.create(user_id: user_three.id, content: "This message is from Dimon");
-message_4 = Message.create(user_id: user_four.id, content: "This message is from Weiner");
+message_4 = Message.create(user_id: user_four.id, content: "This message is from Doge");
 message_5 = Message.create(user_id: user_five.id, content: "This message is from Fry");
 message_6 = Message.create(user_id: user_one.id, content: "BITCOIN");
 message_7 = Message.create(user_id: user_two.id, content: "ETHEREUM");
