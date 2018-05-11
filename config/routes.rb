f43ab40d63207f33eabec005037837672d9a9f68
SaturdayAM <jwl62@georgetown.edu>
2018-05-11 02:24:37 -0400

    got live messaging and updating to work

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post '/signin', to: 'sessions#signin'
   post '/signout', to: 'sessions#signout'
 
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
